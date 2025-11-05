8d2c270eade3419367f21703e4698a2ffd868d09
phgnm <phuongnam710648@gmail.com>
2025-11-05 10:42:01 +0700

    feat: register and login done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AppRouter from "./routes/AppRouter";
+import "./index.css";
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+}
+
+export default App
