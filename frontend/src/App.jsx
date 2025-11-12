e5baf0e2fca6d45292ad8fae7bbe7b11b76ac27f
phgnm <phuongnam710648@gmail.com>
2025-11-12 22:20:38 +0700

    feat: frontend login, account system

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,18 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppRouter from "./routes/AppRouter";
 import "./index.css";
+import { AuthProvider } from "./context/AuthContext";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <AppRouter />
+        </BrowserRouter>
+      </AuthProvider>
     </QueryClientProvider>
   );
 }
--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import AccountPage from "../pages/AccountPage";
+import ProtectedRoute from "../components/ProtectedRoute";
 
 export default function AppRouter() {
     return (
@@ -9,6 +11,10 @@ export default function AppRouter() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+        
+            <Route element={<ProtectedRoute />}>
+                <Route path="/account" element={<AccountPage />} />
+            </Route>
         </Routes>
     )
 }
